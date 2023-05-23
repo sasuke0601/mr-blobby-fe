4175bf97c7ec183842f676d8176622df700ea7fc
sasuke0601 <jkano0601@gmail.com>
2023-05-24 03:00:55 +0900

    home: game section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
 import HeroCover from "../assets/image/hero-cover.jpg";
 import MrBlobbyImg from "../assets/image/mrblobby.png";
+import BubblesImg from "../assets/image/bubbles-lg.png";
+import ArrowBall from "../assets/image/arrow-ball.png";
 
 function Home() {
   return (
@@ -13,10 +15,10 @@ function Home() {
           className="absolute top-0 left-0 object-cover w-full h-full"
           alt=""
         />
-        <div className="container relative z-10 w-full pt-20 mx-auto">
+        <div className="container relative z-10 w-full pt-[120px] mx-auto">
           <img
             src={MrBlobbyImg}
-            className="absolute top-10 left-[100px]"
+            className="absolute top-10 left-[100px] blobby-animation"
             alt=""
           />
           <h1 className="font-primary text-primary text-[100px] font-extrabold ml-[50%] ">
@@ -68,6 +70,37 @@ function Home() {
         </div>
       </div>
       {/* Invest End */}
+
+      {/* Game CTA Beginning */}
+      <div
+        className="relative w-full overflow-hidden "
+        style={{
+          background:
+            "radial-gradient(100% 100% at 50% 0%, #670577 0%, #3A0343 100%)",
+        }}
+      >
+        <div
+          className="h-[290px] -mt-[60px]"
+          style={{ background: `url(${BubblesImg})` }}
+        ></div>
+        <div className="max-w-[760px] mx-auto mt-5 relative pb-[120px]">
+          <h1 className="text-white font-extrabold text-[96px] uppercase leading-[100%] text-shadow">
+            buble
+          </h1>
+          <h1 className="text-white font-extrabold text-[72px] uppercase leading-[100%] text-shadow">
+            shooter
+          </h1>
+          <button className="button-shadow px-8 py-3 uppercase text-[#4B0557] font-extrabold text-[24px] mt-9 bg-white rounded-xl leading-[120%]">
+            play now!
+          </button>
+          <img
+            src={ArrowBall}
+            className="absolute -bottom-[120px] -right-20"
+            alt=""
+          />
+        </div>
+      </div>
+      {/* Game CTA End */}
     </main>
   );
 }
